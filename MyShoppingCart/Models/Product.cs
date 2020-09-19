d3b8845c9c269c3d349b26ef0f202c23b382e010
Shrutam98 <shrutam98@tatvasoft.com>
2020-09-19 15:43:38 +0530

    Initial commit

---
+++ MyShoppingCart/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyShoppingCart.Models
+{
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+        public int? CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double? Price { get; set; }
+        public int? Quantity { get; set; }
+        public double? Discount { get; set; }
+        public double? Gst { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ MyShoppingCart/Models/myShoppingCartContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MyShoppingCart.Models
+{
+    public partial class myShoppingCartContext : DbContext
+    {
+        public myShoppingCartContext()
+        {
+        }
+
+        public myShoppingCartContext(DbContextOptions<myShoppingCartContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=PCA196\\SQL2014;Database=myShoppingCart;User ID=sa;Password=shrutatva123");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.CategoryName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Product)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK__Product__Categor__1920BF5C");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
