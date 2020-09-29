e79ee29c58f63df35e9c1ff2db1c82cc01e4ddd7
Shrutam98 <shrutam98@tatvasoft.com>
2020-09-29 17:45:21 +0530

    code cleanup

--- MyShoppingCart/Controllers/CategoriesController.cs
+++ MyShoppingCart/Controllers/CategoriesController.cs
@@ -42,8 +42,6 @@ namespace MyShoppingCart.Controllers
         }
 
         // PUT: api/Categories/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
         public async Task<IActionResult> PutCategory(int id, Category category)
         {
@@ -74,8 +72,6 @@ namespace MyShoppingCart.Controllers
         }
 
         // POST: api/Categories
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
         public async Task<ActionResult<Category>> PostCategory(Category category)
         {
